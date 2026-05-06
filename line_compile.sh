173b47e440a4de0e04a829a622c97343fc26fe89
Darial <darialdo92@gmail.com>
2026-05-06 13:34:43 -0500

    Create line_compile.sh

---
+++ line_compile.sh
@@ -0,0 +1,1 @@
+g++ get_video.cpp -o display_image -I/usr/local/include/opencv4/ -L/usr/local/lib -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_stitching -lopencv_videoio
