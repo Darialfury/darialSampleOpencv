a043d6a07f7c4b2a9dafb720ced55590c0e30a8e
Darialfury <darialdo92@gmail.com>
2026-05-07 15:37:08 -0500

    fixed for arch linux

--- get_video.cpp
+++ get_video.cpp
@@ -1,38 +1,41 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
- int main() {
-// Create a VideoCapture object and open the default camera (device index 0)
-// Use 1, 2, etc. for additional cameras.
-//cv::VideoCapture cap(0);
-// 192.168.101.78 
-cv::VideoCapture cap("rstp://")
-  // Check if the camera opened successfully.
-if (!cap.isOpened()) {
-std::cerr << "ERROR! Unable to open camera\n";
-return -1;
-}
-  std::cout << "Start grabbing frames from camera" << std::endl;
-std::cout << "Press any key to terminate" << std::endl;
-  // Loop continuously to read frames.
-for (;;) {
-cv::Mat frame;
-	cv::Mat frameGray;
-// Wait for a new frame from the camera and store it into 'frame'.
-cap.read(frame);
-   // Check if the frame is empty (e.g., end of stream).
-if (frame.empty()) {
-std::cerr << "ERROR! blank frame grabbed\n";
-break;
-}
-	cv::cvtColor(frame, frameGray, cv::COLOR_BGR2GRAY);
-// Display the captured frame in a window named "Live".
-cv::imshow("Live", frame);
-	cv::imshow("Gray", frameGray);
-  // Wait for 5ms for a key press. If a key is pressed, break the loop.
-if (cv::waitKey(5) >= 0) {
-break;
-}
-}
+int main() {
+  // Create a VideoCapture object and open the default camera (device index 0)
+  // Use 1, 2, etc. for additional cameras.
+  cv::VideoCapture cap(0);
+  // 192.168.101.78 
+  //cv::VideoCapture cap("rstp://")
+  // Check if the camera opened successfully.
+
+  if (!cap.isOpened()) {
+    std::cerr << "ERROR! Unable to open camera\n";
+    return -1;
+  }
+
+  std::cout << "Start grabbing frames from camera" << std::endl;
+  std::cout << "Press any key to terminate" << std::endl;
+
+  // Loop continuously to read frames.
+  for (;;) {
+    cv::Mat frame;
+	  cv::Mat frameGray;
+    // Wait for a new frame from the camera and store it into 'frame'.
+    cap.read(frame);
+    // Check if the frame is empty (e.g., end of stream).
+    if (frame.empty()) {
+      std::cerr << "ERROR! blank frame grabbed\n";
+      break;
+    }
+	  cv::cvtColor(frame, frameGray, cv::COLOR_BGR2GRAY);
+    // Display the captured frame in a window named "Live".
+    cv::imshow("Live", frame);
+	  cv::imshow("not gray", frameGray);
+    // Wait for 5ms for a key press. If a key is pressed, break the loop.
+    if (cv::waitKey(5) >= 0) {
+      break;
+    }
+  }
+  return 0;
 }// The VideoCapture object is automatically deinitialized in its destructor.
-return 0;
